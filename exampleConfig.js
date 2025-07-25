f7c0287d76ab80d01f9745788362295ace5af373
Paul Wiegand <pwiegand2001@gmail.com>
2025-07-25 20:56:03 +0200

    Add multiple CORS origins

--- exampleConfig.js
+++ exampleConfig.js
@@ -3,6 +3,9 @@ let config = {};
 config.CLIENT_SECRET_GITHUB = "";
 config.CLIENT_SECRET_GITLAB = "";
 config.FRONTEND_URL = "";
+config.CORS_ALLOWED_ORIGINS = [
+    this.FRONTEND_URL,
+];
 config.SERVER_PORT = 1234;
 config.PROXY_PORT = 1235;
 
